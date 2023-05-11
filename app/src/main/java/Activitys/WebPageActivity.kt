14d414d3f5816174c38794121dcf713988c15867
Rio <Rioz2008x@gmail0com>
2023-05-11 22:07:37 +0330

    add activitys page

--- app/src/main/java/Activitys/WebPageActivity.kt
+++ app/src/main/java/Activitys/WebPageActivity.kt
@@ -1,11 +1,12 @@
-package com.example.newsapp
+package Activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsapp.R
 
-class MainActivity : AppCompatActivity() {
+class WebPageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_web_page)
     }
 }
