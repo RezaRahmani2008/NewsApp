4c615e9ad72897d42b9e436ccee2f8f09168c07f
Rio <Rioz2008x@gmail0com>
2023-05-11 23:18:33 +0330

    add fichr 1

--- app/src/main/java/Activitys/WebPageActivity.kt
+++ app/src/main/java/Activitys/WebPageActivity.kt
@@ -3,6 +3,10 @@ package Activitys
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.ProgressBar
+import androidx.core.view.isInvisible
+import androidx.core.view.isVisible
 import com.example.newsapp.R
 
 class WebPageActivity : AppCompatActivity() {
@@ -11,8 +15,14 @@ class WebPageActivity : AppCompatActivity() {
         setContentView(R.layout.activity_web_page)
 
         var webView = findViewById<WebView>(R.id.webView)
+        var progressBar2 = findViewById<ProgressBar>(R.id.progressBar2)
+        progressBar2.isVisible = true
         webView.loadUrl(intent.getStringExtra("url")!!)
-
-
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                progressBar2.isInvisible = true
+                webView.isVisible = true
+            }
+        }
     }
 }
