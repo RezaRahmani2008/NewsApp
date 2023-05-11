0bc3de6f986b47de72e4793a1f42e99282cd2a81
Rio <Rioz2008x@gmail0com>
2023-05-11 22:45:20 +0330

    add api code

--- app/src/main/java/Api/API.kt
+++ app/src/main/java/Api/API.kt
@@ -1,4 +1,10 @@
 package Api
 
-class API {
+
+interface API {
+    @GET("top-headlines")
+    suspend fun getNews(
+        @Query("Sources") source: String,
+        @Query("apiKey") apiKey: String
+    ): Response<ResponseModel>
 }
